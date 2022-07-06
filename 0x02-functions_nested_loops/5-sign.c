a78fc9cbdcd98c07db0a5e0f45eaec3b2181c7cd
Gdigitalz <patrickgodlives@gmail.com>
2022-07-05 20:38:22 -0700

    Your commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,25 @@
 #include "main.h"
+
 /**
- * print_sign - entry point
- * @n : number to check
- * Return:0 or 1
- */
+* print_sign - prints the sign of a number
+* @n: the int to check
+* Return: 1 and prints + if n is greater than zero
+* 0 and prints 0 if n is zero
+* -1 and prints - if n is less than zero
+*/
 int print_sign(int n)
 {
-	int numz;
-
 	if (n > 0)
 	{
-		numz = 1;
 		_putchar('+');
-	}
-	else if (n == 0)
+		return (1);
+	} else if (n == 0)
 	{
-		numz = 0;
-		_putchar('0');
-	}
-	else
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
 	{
-		numz = -1;
 		_putchar('-');
 	}
-	return (numz);
+		return (-1);
 }
