e47b7eb4bd3698a3a36028c79902a8b7e92feef2
Gdigitalz <patrickgodlives@gmail.com>
2022-08-11 10:50:15 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,17 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-if (!h->str)
-printf("[0] (nil)\n");
-else
-			printf("[%u] %s\n", h->len, h->str);
+		if (h->str == NULL)
+/* Dont use the ! operator it says if no h and that's a non ending loop since it wouldn't find that */
+
+			printf("[%d] %s\n", 0, "(nil)");
+	/* This uses the format specifiers to give out the desired result in their desired types if your enclose all in quote as u did it will turn everything within it into a string */
+	else
+			printf("[%d] %s\n", h->len, h->str);
+
+/* Your printing an interger so %d is the desirable specifier*/
 		h = h->next;
 		s++;
 	}
